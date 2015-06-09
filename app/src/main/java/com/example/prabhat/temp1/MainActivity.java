ba34d4dfc221d443b688877688841af6a940c3ec
prabhtz <prabhat49@hotmail.com>
2015-06-09 15:28:27 +0545

    Initial commit

---
+++ app/src/main/java/com/example/prabhat/temp1/MainActivity.java
@@ -0,0 +1,183 @@
+package com.example.prabhat.temp1;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothA2dp;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.widget.ListViewAutoScrollHelper;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+
+public class MainActivity extends Activity {
+
+
+    private static final int ENABLE_BT_REQUEST_CODE = 1;
+    private static final int DISCOVERABLE_BT_REQUEST_CODE = 2;
+    private static final int DISCOVERABLE_DURATION = 400;
+    private ToggleButton toggleButton;
+    private ListView listView;
+    private BluetoothAdapter bAdap;
+    private ArrayAdapter<String> adapter;
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // Whenever a remote Bluetooth device is found
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+                  adapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        listView.setAdapter(adapter);
+
+        bAdap = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public void Toggle(View view){
+        adapter.clear();
+        ToggleButton toggleButton = (ToggleButton) view;
+        if (bAdap == null){
+           Toast tost = Toast.makeText(getApplicationContext(),"NO BLUETOOTH!!!" ,Toast.LENGTH_SHORT);
+           tost.show();
+            toggleButton.setChecked(false);
+        }
+        else{
+            if(toggleButton.isChecked()){
+                if(!bAdap.isEnabled()){
+                    Intent BIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(BIntent, ENABLE_BT_REQUEST_CODE);
+                }
+                else{
+                    Toast tost1 = Toast.makeText(getApplicationContext(),"DEVICE ENABLED", Toast.LENGTH_SHORT);
+                    tost1.show();
+                    discoverDevices();
+                    makeDiscoverable();
+                }
+            }else {
+                bAdap.disable();
+                adapter.clear();
+                Toast.makeText(getApplicationContext(), "DRIVER IS DISABLED.",
+                Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+    }
+
+    //WORKING......
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == ENABLE_BT_REQUEST_CODE) {
+
+            // Bluetooth successfully enabled!
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "Bluetooth is now enabled." +
+                                "\n" + "Scanning for remote Bluetooth devices...",
+                        Toast.LENGTH_SHORT).show();
+
+                // Make local device discoverable by other devices
+                makeDiscoverable();
+
+                // To discover remote Bluetooth devices
+                discoverDevices();
+
+            } else { // RESULT_CANCELED as user refused or failed to enable Bluetooth
+                Toast.makeText(getApplicationContext(), "Bluetooth is not enabled.",
+                        Toast.LENGTH_SHORT).show();
+
+                // Turn off togglebutton
+                toggleButton.setChecked(false);
+            }
+        } else if (requestCode == DISCOVERABLE_BT_REQUEST_CODE){
+
+            if (resultCode == DISCOVERABLE_DURATION){
+                Toast.makeText(getApplicationContext(), "Your device is now discoverable by other devices for " +
+                                DISCOVERABLE_DURATION + " seconds",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Fail to enable discoverability on your device.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    protected void discoverDevices(){
+        // To scan for remote Bluetooth devices
+        if (bAdap.startDiscovery()) {
+            Toast.makeText(getApplicationContext(), "Discovering other bluetooth devices...",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Discovery failed to start.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void makeDiscoverable(){
+        // Make local device discoverable
+        Intent discoverableIntent = new
+                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVERABLE_DURATION);
+        startActivityForResult(discoverableIntent, DISCOVERABLE_BT_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Register the BroadcastReceiver for ACTION_FOUND
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        this.registerReceiver(broadcastReceiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.unregisterReceiver(broadcastReceiver);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
